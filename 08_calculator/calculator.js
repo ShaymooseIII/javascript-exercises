ed44e2a426d0243758eea06c40d9afdbb4673361
Seamus Xubuntu <151066590+ShaymooseIII@users.noreply.github.com>
2024-09-13 16:01:58 -0700

    completion of the eighth

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let answer = 0
+  for(const num of array){
+    answer += num
+  }
+	return answer
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let answer = 1
+  for(const num of array){
+    answer = answer * num
+  }
+  return answer
 
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+  return num1**num2
+  
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num ===0){
+    return 1
+  }else{
+    for(let i = num - 1; i >= 1; i--){
+      num = num * i
+    }
+    return num
+  }
 };
 
 // Do not edit below this line
