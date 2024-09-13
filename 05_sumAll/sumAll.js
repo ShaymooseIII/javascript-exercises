1d3e4830256f479db83a4e681c862a466ff53bee
Seamus Xubuntu <151066590+ShaymooseIII@users.noreply.github.com>
2024-09-13 14:33:44 -0700

    completion of fifth

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    
+    if(num1 < 0 || num2 <0 || typeof(num1) !== "number" || typeof(num2) !== "number" || num1%1 != 0 || num2%1 != 0){
+        return "ERROR"
+    }
+    if(num1<num2){
+        return(num2*(num2+1))/2
+    }else{
+        return(num1*(num1+1))/2
+    }
 
 };
 
