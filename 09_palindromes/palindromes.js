88267d93a6a27e965e355d12b42340114493e144
Seamus Xubuntu <151066590+ShaymooseIII@users.noreply.github.com>
2024-09-13 16:33:15 -0700

    completion of the ninth

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // creating list of punctuation to filter out
+    punctuation = [".", "!", ",", " "]
+    // lowercasing the string and creating an array
+    array = string.toLowerCase().split("")
+    // filtering the array 
+    filterdArray = array.filter(x => !punctuation.includes(x))
+    //creation of the two variables to compare
+    forward = filterdArray.join("")
+    backwards = filterdArray.reverse().join("")
+    // return condition  
+    return forward === backwards
 };
 
 // Do not edit below this line
