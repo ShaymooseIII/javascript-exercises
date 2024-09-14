0096a8ebaef99b5ae158155e80f616bbe61471c2
Seamus Xubuntu <151066590+ShaymooseIII@users.noreply.github.com>
2024-09-13 20:59:04 -0700

    completion of the twelfth

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = 0;
+    let ages = {};
+    for(person of people){
+        if(!person.yearOfDeath){
+            person.yearOfDeath = new Date().getFullYear()
+        }
+        age = person["yearOfDeath"] - person["yearOfBirth"];
+        ages[age] = person["name"];
+        if(age > maxAge){
+            maxAge = age;
+        }
+    }
+        let answer = people.filter(person =>{
+            return person.name === ages[maxAge]
+        })
+    return answer[0]
 };
 
 // Do not edit below this line
